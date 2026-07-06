5fd114db8ca9e879a58a347e8a5f7e7a5b0f42f0
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2026-07-06 22:35:03 +0200

    Regenerate stubs after cleanup merge

--- src/autogen/stubs/stubs_part010.cpp
+++ src/autogen/stubs/stubs_part010.cpp
@@ -1147,38 +1147,6 @@ undefined4 InitializeChildWindowSurfaceAndTickTimer_Impl_At00482820(void)
   return 0;
 }
 
-// ghidra_name TCViewOwnedBufferRegistryState_00648560::ConstructCViewOwnedBufferRegistryState
-// ghidra_proto undefined ConstructCViewOwnedBufferRegistryState()
-// FUNCTION: IMPERIALISM 0x00482950
-undefined4 ConstructCViewOwnedBufferRegistryState(void)
-{
-  return 0;
-}
-
-// ghidra_name TCViewOwnedBufferChainState_00648578::WrapperFor_FreeHeapBufferIfNotNull_At004829f0
-// ghidra_proto undefined WrapperFor_FreeHeapBufferIfNotNull_At004829f0()
-// FUNCTION: IMPERIALISM 0x004829f0
-undefined4 WrapperFor_FreeHeapBufferIfNotNull_At004829f0(void)
-{
-  return 0;
-}
-
-// ghidra_name TCViewOwnedBufferChainState_00648578::DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At00482a20
-// ghidra_proto undefined DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At00482a20()
-// FUNCTION: IMPERIALISM 0x00482a20
-undefined4 DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At00482a20(void)
-{
-  return 0;
-}
-
-// ghidra_name TCViewOwnedBufferRegistryState_00648560::DestructCViewOwnedBufferRegistryStateAndReleaseOwnedBuffers
-// ghidra_proto undefined DestructCViewOwnedBufferRegistryStateAndReleaseOwnedBuffers()
-// FUNCTION: IMPERIALISM 0x00482ab0
-undefined4 DestructCViewOwnedBufferRegistryStateAndReleaseOwnedBuffers(void)
-{
-  return 0;
-}
-
 // ghidra_name OrphanVtableAssignStub_00482bd0
 // ghidra_proto undefined OrphanVtableAssignStub_00482bd0()
 // FUNCTION: IMPERIALISM 0x00482bd0
@@ -1203,7 +1171,7 @@ undefined4 Helper_Uses_BlitSurfaceRectSkippingTransparentColor_At00482ed0(void)
   return 0;
 }
 
-// ghidra_name TCViewOwnedBufferRegistryState_00648560::UpdateAndRenderMapTileHintOverlayQueue
+// ghidra_name CIncludeView::UpdateAndRenderMapTileHintOverlayQueue
 // ghidra_proto undefined UpdateAndRenderMapTileHintOverlayQueue()
 // FUNCTION: IMPERIALISM 0x00482fc0
 undefined4 UpdateAndRenderMapTileHintOverlayQueue(void)
@@ -1303,30 +1271,6 @@ undefined4 WrapperFor_GetWindowExStyleViaSiteOrHandle_At004840d0(void)
 // ghidra_proto undefined DereferencePointerCellInPlace()
 // FUNCTION: IMPERIALISM 0x004845f0
 undefined4 DereferencePointerCellInPlace(void)
-{
-  return 0;
-}
-
-// ghidra_name SerializeRecordList_0x18_WithBlockPool
-// ghidra_proto undefined SerializeRecordList_0x18_WithBlockPool()
-// FUNCTION: IMPERIALISM 0x00484610
-undefined4 SerializeRecordList_0x18_WithBlockPool(void)
-{
-  return 0;
-}
-
-// ghidra_name WrapperFor_FreeHeapBufferIfNotNull_At004847a0
-// ghidra_proto undefined WrapperFor_FreeHeapBufferIfNotNull_At004847a0()
-// FUNCTION: IMPERIALISM 0x004847a0
-undefined4 WrapperFor_FreeHeapBufferIfNotNull_At004847a0(void)
-{
-  return 0;
-}
-
-// ghidra_name TCViewOwnedBufferChainState_00648578::DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At004847d0
-// ghidra_proto undefined DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At004847d0()
-// FUNCTION: IMPERIALISM 0x004847d0
-undefined4 DestructCViewOwnedBufferChainState_00648578_AndFreeChain_At004847d0(void)
 {
   return 0;
 }
@@ -1599,14 +1543,6 @@ undefined4 DispatchTurnEventPacketWithCodeAndPayloadBuffer_Impl(void)
 // ghidra_proto undefined WrapperFor_ftol_At00489a20()
 // FUNCTION: IMPERIALISM 0x00489a20
 undefined4 WrapperFor_ftol_At00489a20(void)
-{
-  return 0;
-}
-
-// ghidra_name ClearQuickDrawModeFlag950AC
-// ghidra_proto undefined ClearQuickDrawModeFlag950AC()
-// FUNCTION: IMPERIALISM 0x00489a90
-undefined4 ClearQuickDrawModeFlag950AC(void)
 {
   return 0;
 }
